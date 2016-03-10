fac3888ff140e009da20de63b0825c591cca789a
praktan20 <praktan20@gmail.com>
2016-03-09 19:53:58 -0800

    first commit

---
+++ Networking/tcpclient.py
@@ -0,0 +1,20 @@
+import socket
+
+def main():
+	host = '127.0.0.1'
+	port = 5000
+	s = socket.socket()
+	
+	s.connect((host, port))
+	
+	message = raw_input('-> ')
+	while message!= 'q':
+		s.send(message)
+		data = s.recv(65535)
+		print 'Recieved from server: ' + str(data)
+		message = raw_input('-> ')
+	s.close()
+
+if __name__== '__main__':
+	main()
+
